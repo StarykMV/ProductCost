49a2e4d3578ad679736c8e263080e6914ee887e4
Miroslav <myroslav.staryk@viti.edu.ua>
2025-04-21 18:38:32 +0300

    Added MEASURE and CURRENCY constants to the Constants class

--- src/app/Constants.java
+++ src/app/Constants.java
@@ -2,4 +2,8 @@ package app;
 
 public class Constants {
     public static final double DELIVERY_COST = 7.0;
+
+    public static final String MEASURE = " units";
+    public static final String CURRENCY = " $";
 }
+
